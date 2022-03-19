37f7663ad4c6beb5dd27d9ccc930c25512d6e9b0
Josh Carlson <josh.carlson4061@gmail.com>
2022-03-19 11:19:10 -0700

    update folder structure and remove 3D stuff/nodes from project

--- scripts/App.gd
+++ scripts/App.gd
@@ -4,7 +4,7 @@ extends Node
 # hides main menu and loads proper level
 func play_game():
 	$MainMenu.visible = false
-	add_child(load("res://assets/nodes/levels/Level0.tscn").instance())
+	add_child(load("res://nodes/levels/Level0.tscn").instance())
 
 # hides main menu and shows help screen
 func help_menu():
