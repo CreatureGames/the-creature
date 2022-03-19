61d255bad29b9e989b3d261978ad281d48a4a9d9
Rylan Schubkegel <rschubkegel17@georgefox.edu>
2022-03-19 11:10:37 -0700

    Add basic project structure

---
+++ assets/scripts/App.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+# hides main menu and loads proper level
+func play_game():
+	$MainMenu.visible = false
+	add_child(load("res://assets/nodes/levels/Level0.tscn").instance())
+
+# hides main menu and shows help screen
+func help_menu():
+	$MainMenu.visible = false
+
+# saves game data and quits
+func exit():
+	get_tree().quit()
---
+++ assets/scripts/MainMenu.gd
@@ -0,0 +1,16 @@
+extends CenterContainer
+
+signal play_game
+signal help_menu
+signal exit
+
+
+
+func play_pressed():
+	emit_signal("play_game")
+
+func help_pressed():
+	emit_signal("help_menu")
+
+func exit_pressed():
+	emit_signal("exit")
