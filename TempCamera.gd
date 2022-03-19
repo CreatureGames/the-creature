c27f2e767a3c61e648bf19e9c9c8f68d5484246e
Rylan Schubkegel <rylanschubkegel@gmail.com>
2022-03-19 13:33:02 -0700

    Add basic parallaxing to level 0

---
+++ TempCamera.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+export(int, 100) var speed
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed * delta
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed * delta
