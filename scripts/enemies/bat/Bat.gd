333dff8d4edf426b79fc1840811c7a76a3b39335
Rylan Schubkegel <rylanschubkegel@gmail.com>
2022-03-20 12:37:24 -0700

    Bat moves when attacking

--- scripts/enemies/bat/Bat.gd
+++ scripts/enemies/bat/Bat.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
 
 
+export var follow_speed : int
+export var attack_speed : int
+
+
 # someone's near...
 func detection_zone_entered(area) -> void:
 	# wake up if sleeping
