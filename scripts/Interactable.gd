199cce22656c92cc8d46509df9515760a17d92fc
Josh Carlson <josh.carlson4061@gmail.com>
2022-03-22 10:02:38 -0700

    enter/exit signals for interactables

--- scripts/Interactable.gd
+++ scripts/Interactable.gd
@@ -2,11 +2,15 @@ extends Area2D
 
 var partners = []
 signal on_interact
+signal interact_enter
+signal interact_exit
 
 func interact_entered(area):
+	emit_signal("interact_enter")
 	partners.append(area)
 
 func interact_exited(area):
+	emit_signal("interact_exit")
 	partners.erase(area)
 
 func _interact():
