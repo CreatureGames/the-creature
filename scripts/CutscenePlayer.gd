f133b8e2f384bf3ae784b009013959e3e4c4e01d
Rylan Schubkegel <rylanschubkegel@gmail.com>
2022-03-24 09:38:03 -0700

    Polish first cutscene

--- scripts/CutscenePlayer.gd
+++ scripts/CutscenePlayer.gd
@@ -1,10 +1,12 @@
 extends AnimationPlayer
 
 export var cutscene := "IntroCutscene"
+var played = false
 
 func enter_trigger(body):
-	if body is PlayerRobot:
+	if body is PlayerRobot and not played:
 		play_cutscene(cutscene)
+		played = true
 
 func play_cutscene(scene: String):
 	play(scene)
